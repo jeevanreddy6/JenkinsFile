e0528c5c583be67b754d84ea3d39607babbb62d6
unknown <JeevanReddy.Yemireddy@lntinfotech.com>
2020-09-10 15:24:38 +0530

    folder

---
+++ Sampleproj/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sampleproj
+{
+    class Program
+    {
+        void GetNumbers() 
+        {
+            int iNum1 = 0, iNum2 = 0;
+            iNum1 = int.MaxValue;
+            Console.WriteLine("The value of Num1 is " + iNum1);
+            iNum1++;
+            Console.WriteLine("The value of Num1 after increment is " + iNum1);
+            
+        }
+        static void Main(string[] args)
+        {
+            new Program().GetNumbers();
+            Console.ReadKey();
+        }
+    }
+}
